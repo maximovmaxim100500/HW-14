05fe599ca7271c60cfad0ec5d94658e8080cecac
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-12-13 22:58:04 +0300

    Домашка готова

---
+++ src/PossibilitiesServiceStation.java
@@ -0,0 +1,13 @@
+public interface PossibilitiesServiceStation {
+    public void check(Car car);
+
+    public void check(Truck truck);
+
+    public void check(Bicycle bicycle);
+
+    public void updateTyre();
+
+    public void checkEngine();
+
+    public void checkTrailer();
+}
--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,23 +1,45 @@
-public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
-            }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
+public class ServiceStation implements PossibilitiesServiceStation {
+    @Override
+    public void check(Car car) {
+        System.out.println("Обслуживаем " + car.getModelName());
+        for (int i = 0; i < car.getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
+    }
+
+    @Override
+    public void check(Truck truck) {
+        System.out.println("Обслуживаем " + truck.getModelName());
+        for (int i = 0; i < truck.getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
+        checkTrailer();
+    }
+
+    @Override
+    public void check(Bicycle bicycle) {
+        System.out.println("Обслуживаем " + bicycle.getModelName());
+        for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+            updateTyre();
         }
     }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
 }
+
+
